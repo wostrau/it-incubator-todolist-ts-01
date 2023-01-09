0d61a5fb24a29af9a4eed2561b865e0832fc4960
wostrau <a.ostrovtsov@gmail.com>
2023-01-09 20:50:30 +0100

    Todolist 13 - TBD (stopped at 01:10:00)

--- src/state/todolists-api.stories.tsx
+++ src/state/todolists-api.stories.tsx
@@ -6,7 +6,10 @@ export default {
 };
 
 const settings = {
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'API-KEY': '96e14868-2995-4951-a0b1-5ff5cded4fa9'
+    }
 };
 
 export const GetTodolists = () => {
@@ -21,15 +24,23 @@ export const GetTodolists = () => {
 };
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null);
+    const payload = {title: 'New todolist sent from storybook'}
     useEffect(() => {
-
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', payload, settings)
+            .then((res) => {
+                setState(res.data);
+            });
     }, []);
     return <div>{JSON.stringify(state)}</div>
 };
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null);
+    const todolistId = 'd9ac3bef-fb2c-49b8-b201-e0c805ac01da';
     useEffect(() => {
-
+        axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`, settings)
+            .then((res) => {
+                setState(res.data);
+            });
     }, []);
     return <div>{JSON.stringify(state)}</div>
 };
