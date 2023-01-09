25a8014a08880e7eb5a1d149f0a60e451b9e5a1c
wostrau <a.ostrovtsov@gmail.com>
2023-01-09 20:22:28 +0100

    Todolist 13 - TBD (stopped at 55:00)

---
+++ src/state/todolists-api.stories.tsx
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+export default {
+    title: 'API'
+};
+
+const settings = {
+    withCredentials: true
+};
+
+export const GetTodolists = () => {
+    const [state, setState] = useState<any>(null);
+    useEffect(() => {
+        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+            .then((res) => {
+                setState(res.data);
+            });
+    }, []);
+    return <div>{JSON.stringify(state)}</div>
+};
+export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null);
+    useEffect(() => {
+
+    }, []);
+    return <div>{JSON.stringify(state)}</div>
+};
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null);
+    useEffect(() => {
+
+    }, []);
+    return <div>{JSON.stringify(state)}</div>
+};
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null);
+    useEffect(() => {
+
+    }, []);
+    return <div>{JSON.stringify(state)}</div>
+};
