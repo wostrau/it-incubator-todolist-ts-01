2bc721974115646db11377d8f4c690fe28b52ab5
wostrau <a.ostrovtsov@gmail.com>
2023-01-18 23:43:53 +0100

    Todolist 17 - TBD (stopped at 20:00)

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
-import {applyMiddleware, combineReducers, legacy_createStore as createStore} from 'redux';
+import {combineReducers} from 'redux';
 import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk';
 import {TodolistsActionsType, todolistsReducer} from '../features/TodolistList/Todolist/todolists-reducer';
 import {TasksActionsType, tasksReducer} from '../features/TodolistList/Todolist/Task/tasks-reducer';
 import {AppActionsType, appReducer} from './app-reducer';
-import {LoginActionsType, authReducer} from '../features/Login/auth-reducer';
+import {authReducer, LoginActionsType} from '../features/Login/auth-reducer';
+import {configureStore} from '@reduxjs/toolkit';
 
 const rootReducer = combineReducers({
     todolists: todolistsReducer,
@@ -12,7 +13,11 @@ const rootReducer = combineReducers({
     auth: authReducer,
 });
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunk),
+});
+//export const store = createStore(rootReducer, applyMiddleware(thunk));
 
 // types
 export type AppRootStateType = ReturnType<typeof store.getState>;
